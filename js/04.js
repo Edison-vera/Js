eb20ecdbb7c9e925494aa6b460c03fa755441818
Edison-vera <edisonefvc15@gmail.com>
2022-01-31 20:24:37 -0500

    String methods

--- js/04.js
+++ js/04.js
@@ -1,10 +1,21 @@
 // String o cadenas de texto 
 
 const producto = "Monitor de 20\""
-const producto2 = String("Monitor de 30 pulgadas");
-const producto3 = new String("Monitor de 50 pulgadas");
+const producto2 = "Monitor HD"
 
+// const producto2 = String("Monitor de 30 pulgadas");
+// const producto3 = new String("Monitor de 50 pulgadas");
 
-console.log(producto);
-console.log(producto2);
-console.log(producto3);
+// Length es para la extension
+console.log(producto.length);
+console.log(producto2.length);
+// console.log(producto3);
+
+// Indexof (Retorna a la posicion)
+console.log(producto.indexOf("Monitor"));
+console.log(producto2.indexOf("monitor"));
+
+// Includes (Retorna true o false)
+
+console.log(producto.includes("Monitor"));
+console.log(producto2.includes("monitor"));
