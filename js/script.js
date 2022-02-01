ccc5e0ad61ed738c61db26b16528db4af94e4a50
Edison-vera <edisonefvc15@gmail.com>
2022-01-31 19:47:57 -0500

    Js variable

---
+++ js/script.js
@@ -0,0 +1,24 @@
+// Variables 
+
+
+var producto = "Audifonos Gamer"; // Iniciar variables y asignar valor 
+
+var disponible; //Iniciamos variable pero sin valor
+
+// producto = true; //Reasignamos el valor de la variable
+
+disponible = true;
+
+var producto1 = "Computadora",
+    disponible1 = true,
+    categoria = "Computadora";
+
+//Estilos para las variables
+
+var nombre_producto = "Monitor HD"; // underscore
+var nombreProducto = "Monitor HD"; // Camelcase
+var NombreProducto = "Monitor HD"; // Pascal Case
+var nombreproducto = "Monitor HD"; // 
+
+
+console.log(producto);
