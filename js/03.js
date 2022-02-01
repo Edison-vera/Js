85503dee62165ecf905e92a78ee636af957c151a
Edison-vera <edisonefvc15@gmail.com>
2022-01-31 20:06:00 -0500

    Js variable const, led

---
+++ js/03.js
@@ -0,0 +1,27 @@
+//Variables con const
+
+
+
+
+
+const producto = "Audifonos Gamer"; // Iniciar variables y asignar valor 
+
+let disponible = true; //Iniciamos variable pero sin valor
+
+// producto = true; //Reasignamos el valor de la variable
+
+disponible = true;
+
+const producto1 = "Computadora",
+    disponible1 = true,
+    categoria = "Computadora";
+
+//Estilos para las variables
+
+const nombre_producto = "Monitor HD"; // underscore
+const nombreProducto = "Monitor HD"; // Camelcase
+const NombreProducto = "Monitor HD"; // Pascal Case
+const nombreproducto = "Monitor HD"; // 
+
+
+console.log(producto);
