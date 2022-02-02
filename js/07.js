202d34f06fd6f8cd324c0be6b7a3df230b49f06d
Edison-vera <edisonefvc15@gmail.com>
2022-02-01 19:51:00 -0500

    Template String

---
+++ js/07.js
@@ -0,0 +1,21 @@
+// Orden de las operaciones
+
+let resultado;
+
+resultado = 20 + 30 * 2;
+resultado = (20 + 30) * 2;
+
+
+// console.log(resultado)
+
+//Incrementos 
+
+
+let puntaje = 10;
+
+puntaje++;
+++puntaje;
+puntaje--;
+puntaje += 10;
+
+console.log(puntaje++);
---
+++ js/08.js
@@ -0,0 +1,13 @@
+const nombre = "juan";
+const email = "correo@correo.com";
+
+
+//Concatenacion
+
+console.log("Nombre cliente: " + nombre + " Email: " + email);
+
+
+//Template sitring
+
+
+console.log(`Nombre de cliente: ${nombre} Email: ${email}`);
