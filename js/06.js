0ff1e13f2dc6ef98fbfa172f8f267aeedf9c081f
Edison-vera <edisonefvc15@gmail.com>
2022-02-01 19:31:50 -0500

    Math

---
+++ js/06.js
@@ -0,0 +1,17 @@
+// Obteto math
+
+let resultado;
+
+resultado = Math.PI;
+resultado = Math.round(52.2);
+resultado = Math.ceil(52.9); // Ceil redondea hacia arriba 
+resultado = Math.floor(52.9); // Floor redondea hacia abajo 
+resultado = Math.sqrt(52);
+resultado = Math.abs(-52.9);
+resultado = Math.min(52.9, 5, 4, 2, 3, 1);
+resultado = Math.max(52.9, 5, 4, 2, 3, 1);
+resultado = Math.random();
+resultado = Math.floor(Math.random() * 30);
+
+
+console.log(resultado);
