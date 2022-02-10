317c4910665819e042d96ab1d0dceb52d6058bc9
Edison-vera <edisonefvc15@gmail.com>
2022-02-09 19:41:10 -0500

    Fetch API

---
+++ js/33.js
@@ -0,0 +1,26 @@
+async function obtenerEmpleados() {
+
+    const archivo = "empleados.json";
+    // fetch(archivo)
+    //     .then(resultado => resultado.json())
+    //     .then(datos => {
+    //         console.log(datos)
+
+    //         const { empleados } = datos;
+    //         console.log(empleados);
+    //         // empleados.forEach(empleados => {
+    //         //     console.log(empleados.id);
+    //         //     console.log(empleados.nombre);
+    //         //     console.log(empleados.puesto);
+
+    //         //     document.querySelector(`.contenido`).textContent += empleados.nombre;
+
+    //     });
+
+    const resultado = await fetch(archivo);
+    console.log(resultado);
+    const datos = await resultado.json();
+    console.log(datos);
+}
+
+obtenerEmpleados();
